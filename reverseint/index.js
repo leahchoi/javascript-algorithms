71f8b93c702fd281f05dd5b16f542f271261f0e9
leahchoi <soora913@gmail.com>
2019-01-03 18:01:45 -0800

    solved reverseint

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    var output = null;
+    if (n === 0) {
+        return n;
+    }
+    if (n < 0) {
+        output = '-';
+        n *= -1;
+    }
+    n = n.toString();
+    for (let i = n.length - 1; i >= 0; i--) {
+        if ((output === null || output === '-') && n[i] === "0") {
+        } else if (output === null) {
+            output = n[i];
+        } else {
+            output += n[i];
+        }
+    }
+    return parseInt(output);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     var reversed = n.toString().split('').reverse().join('');
+//     return parseInt(reversed) * Math.sign(n);
+// }
